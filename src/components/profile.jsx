035f1141dd93e79750120ad3464709b0ecd22b9f
Abdurrahman Rahim <imanabdurrahmanr@gmail.com>
2023-05-29 12:33:45 +0800

    sassahsassah

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -6,8 +6,10 @@ const Profile = ({ img }) => {
             <Box className='photo-containers'>
                 <Box component='img' src={img} width='175px' />
             </Box>
-            <Typography color='text.main' fontSize='0.8rem' pt={2} fontWeight={500}>M. Thariq Ramadhan</Typography>
-            <Typography color='primary.main' fontSize='0.7rem' fontWeight={700}>Ketua Umum</Typography>
+            <Box width='200px'>
+                <Typography color='text.main' fontSize='0.8rem' pt={2} fontWeight={500}>M. Thariq Ramadhan</Typography>
+                <Typography color='primary.main' fontSize='0.7rem' fontWeight={700}>Ketua Umum</Typography>
+            </Box>
         </Box>
     );
 }
